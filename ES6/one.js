5d1d6d7fd32ae386ae56ed94b6a8a152847e5015
Basawraj07 <basumat323@gmail.com>
2023-10-20 14:08:09 +0530

    tst

---
+++ ES6/one.js
@@ -0,0 +1,9 @@
+let enames=["rahul","sonia","priyanka","modi"]
+
+let new_names = []
+
+for(ename of enames){
+    new_names.push(ename.toUpperCase())
+}
+console.log(enames)
+console.log(new_names)
